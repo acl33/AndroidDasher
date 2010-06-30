69034c9ea7db4e4f3d8026a94491eec6eb836ed0
Alan Lawrence <acl33@inf.phy.cam.ac.uk>
2010-06-30 20:46:19 +0100

    A whole raft of suboptions which popup whenever either checkbox is activated

--- src/dasher/android/DasherActivity.java
+++ src/dasher/android/DasherActivity.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
 import android.util.Log;
+import android.widget.CheckBox;
 import dasher.Esp_parameters;
 
 public class DasherActivity extends PreferenceActivity {
   public static final String EYETRACKER_NAME = "Tap-to-start w/ remapping";
-	private ADasherInterface intf;
-	private DasherCanvas surf;
+  
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,25 +24,29 @@ public class DasherActivity extends PreferenceActivity {
         
         IMDasherInterface.INSTANCE.Realize(this);
         
-        final CheckBoxPreference tilt = (CheckBoxPreference)getPreferenceScreen().findPreference("AndroidTilt");
-        final CheckBoxPreference touch =(CheckBoxPreference)getPreferenceScreen().findPreference("AndroidTouch");
-        Preference.OnPreferenceChangeListener chg = new Preference.OnPreferenceChangeListener() {
-        	public boolean onPreferenceChange(Preference p, Object value) {
-        		if (!((Boolean)value).booleanValue()) return false; //disallow deactivation 
-        		if (p==tilt) {
-        			touch.setChecked(false);
+        final PreferenceScreen tiltScr = (PreferenceScreen)getPreferenceScreen().findPreference("AndroidTilt");
+        tiltScr.setWidgetLayoutResource(R.layout.checkbox_tilt);
+        final PreferenceScreen touchScr = (PreferenceScreen)getPreferenceScreen().findPreference("AndroidTouch");
+        touchScr.setWidgetLayoutResource(R.layout.checkbox_touch);
+        Preference.OnPreferenceClickListener clk = new Preference.OnPreferenceClickListener() {
+        	public boolean onPreferenceClick(Preference p) {
+        		if (p==tiltScr) {
+        			tiltCheckBox().setChecked(true);
+        			touchCheckBox().setChecked(false);
         			IMDasherInterface.INSTANCE.SetStringParameter(Esp_parameters.SP_INPUT_DEVICE,"Tilt Input");
         			IMDasherInterface.INSTANCE.SetStringParameter(Esp_parameters.SP_INPUT_FILTER,EYETRACKER_NAME);
         		} else {
-        			tilt.setChecked(false);
+        			tiltCheckBox().setChecked(false);
+        			touchCheckBox().setChecked(true);
         			IMDasherInterface.INSTANCE.SetStringParameter(Esp_parameters.SP_INPUT_DEVICE,"Touch Input");
         			IMDasherInterface.INSTANCE.SetStringParameter(Esp_parameters.SP_INPUT_FILTER,"Stylus Control");
         		}
-        		return true; //...and set.
+        		return false; //Not handled - want ordinary behaviour (popup) to happen next!
         	}
         };
-        tilt.setOnPreferenceChangeListener(chg);
-        touch.setOnPreferenceChangeListener(chg);
+        tiltScr.setOnPreferenceClickListener(clk);
+        touchScr.setOnPreferenceClickListener(clk);
+        
         addPermittedValues(Esp_parameters.SP_ALPHABET_ID);
     }
     
@@ -58,13 +62,26 @@ public class DasherActivity extends PreferenceActivity {
         lp.setEntries(vals);
         lp.setEntryValues(vals);
     }
-    private static final double RR2 = 1.0/Math.sqrt(2.0);
 	
     @Override
     public void onDestroy() {
     	Log.d("DasherIME","Activity onDestroy");
     	super.onDestroy();
     	//intf.StartShutdown();
+    }
+    
+    /**Cache components retrieved by findViewById*/
+    private CheckBox m_tilt,m_touch;
+    /** Get the tilt/touch checkbox - done lazily, as I'm not sure when Preferences inflate their widget layout resources...*/
+    private CheckBox tiltCheckBox() {
+    	if (m_tilt==null)
+    		m_tilt = (CheckBox)DasherActivity.this.findViewById(R.id.checkbox_tilt);
+    	return m_tilt;
+    }
+    private CheckBox touchCheckBox() {
+    	if (m_touch==null)
+    		m_touch = (CheckBox)DasherActivity.this.findViewById(R.id.checkbox_touch);
+    	return m_touch;
     }
     
 }
